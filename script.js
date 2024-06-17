c20ed0a3454c4c4d14dc5b9772f1b23955e0df3d
Diwakar9214 <112749849+Diwakar9214@users.noreply.github.com>
2024-06-17 07:02:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById("Timer").innerText="10"
+    setTimeout(() => {
+      document.getElementById("Timer").innerText="9"
+      setTimeout(() => {
+        document.getElementById("Timer").innerText="8"
+        setTimeout(() => {
+          document.getElementById("Timer").innerText="7"
+          setTimeout(() => {
+            document.getElementById("Timer").innerText="6"
+            setTimeout(() => {
+              document.getElementById("Timer").innerText="5"
+              setTimeout(() => {
+                document.getElementById("Timer").innerText="4"
+                setTimeout(() => {
+                  document.getElementById("Timer").innerText="3"
+                  setTimeout(() => {
+                    document.getElementById("Timer").innerText="2"
+                    setTimeout(() => {
+                      document.getElementById("Timer").innerText="1"
+                      setTimeout(() => {
+                        document.getElementById("Timer").innerText="Happy Independence Day!!!"
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
